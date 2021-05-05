e242e679f39932c0b9cd13b7f08dedba71e3d227
bbimber <bbimber@gmail.com>
2021-05-05 11:50:09 -0700

    Full pass -pedValidation type to GATK

--- src/test/java/com/github/discvrseq/walkers/variantqc/VariantQCIntegrationTest.java
+++ src/test/java/com/github/discvrseq/walkers/variantqc/VariantQCIntegrationTest.java
@@ -2,8 +2,11 @@ package com.github.discvrseq.walkers.variantqc;
 
 import com.github.discvrseq.walkers.BaseIntegrationTest;
 import htsjdk.samtools.util.IOUtil;
+import org.broadinstitute.hellbender.exceptions.UserException;
 import org.broadinstitute.hellbender.testutils.ArgumentsBuilder;
 import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
+import org.broadinstitute.hellbender.utils.samples.PedigreeValidationType;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.BufferedReader;
@@ -11,11 +14,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class VariantQCIntegrationTest extends BaseIntegrationTest {
-    @Test
-    public void testBasicOperation() throws Exception {
-        File expected = generateCompleteOutput(getTestFile("testBasicOperation.html"));
+    private ArgumentsBuilder getBaseArgs(boolean limitToChr1) {
         ArgumentsBuilder args = new ArgumentsBuilder();
         args.addRaw("--variant");
         File input = new File(testBaseDir, "ClinvarAnnotator.vcf");
@@ -25,14 +27,23 @@ public class VariantQCIntegrationTest extends BaseIntegrationTest {
         args.addRaw("-R");
         args.addRaw(normalizePath(fasta));
 
-        args.addRaw("-L");
-        args.addRaw("1");
+        if (limitToChr1) {
+            args.addRaw("-L");
+            args.addRaw("1");
+        }
 
         args.addRaw("-O");
         args.addRaw("%s");
         args.addRaw("--tmp-dir");
         args.addRaw(getTmpDir());
 
+        return args;
+    }
+
+    @Test
+    public void testBasicOperation() throws Exception {
+        File expected = generateCompleteOutput(getTestFile("testBasicOperation.html"));
+        ArgumentsBuilder args = getBaseArgs(true);
         IntegrationTestSpec spec = new IntegrationTestSpec(
                 args.getString(), Arrays.asList(expected.getPath()));
 
@@ -43,22 +54,10 @@ public class VariantQCIntegrationTest extends BaseIntegrationTest {
     @Test
     public void testMaxContigs() throws Exception {
         File expected = generateCompleteOutput(getTestFile("testMaxContigs.html"));
-        ArgumentsBuilder args = new ArgumentsBuilder();
-        args.addRaw("--variant");
-        File input = new File(testBaseDir, "ClinvarAnnotator.vcf");
-        args.addRaw(normalizePath(input));
-
-        File fasta = getHg19Micro();
-        args.addRaw("-R");
-        args.addRaw(normalizePath(fasta));
+        ArgumentsBuilder args = getBaseArgs(false);
 
         args.addRaw("-maxContigs");
         args.addRaw("1");
-
-        args.addRaw("-O");
-        args.addRaw("%s");
-        args.addRaw("--tmp-dir");
-        args.addRaw(getTmpDir());
 
         IntegrationTestSpec spec = new IntegrationTestSpec(
                 args.getString(), Arrays.asList(expected.getPath()));
@@ -70,24 +69,10 @@ public class VariantQCIntegrationTest extends BaseIntegrationTest {
     @Test
     public void testBasicOperationWithRawData() throws Exception {
         File expected = generateCompleteOutput(getTestFile("testBasicOperation.html"));
-        ArgumentsBuilder args = new ArgumentsBuilder();
-        args.addRaw("--variant");
-        File input = new File(testBaseDir, "ClinvarAnnotator.vcf");
-        args.addRaw(normalizePath(input));
+        ArgumentsBuilder args = getBaseArgs(true);
 
-        File fasta = getHg19Micro();
-        args.addRaw("-R");
-        args.addRaw(normalizePath(fasta));
-
-        args.addRaw("-L");
-        args.addRaw("1");
-
-        args.addRaw("-O");
-        args.addRaw("%s");
         args.addRaw("-rd");
         args.addRaw("%s");
-        args.addRaw("--tmp-dir");
-        args.addRaw(getTmpDir());
 
         IntegrationTestSpec spec = new IntegrationTestSpec(
                 args.getString(), Arrays.asList(expected.getPath(), getTestFile("testBasicOperation.json").getPath()));
@@ -147,26 +132,11 @@ public class VariantQCIntegrationTest extends BaseIntegrationTest {
     @Test
     public void testExtendedReports() throws Exception {
         File expected = generateCompleteOutput(getTestFile("testExtendedReports.html"));
-        ArgumentsBuilder args = new ArgumentsBuilder();
-        args.addRaw("--variant");
-        File input = new File(testBaseDir, "ClinvarAnnotator.vcf");
-        args.addRaw(normalizePath(input));
-
-        File fasta = getHg19Micro();
-        args.addRaw("-R");
-        args.addRaw(normalizePath(fasta));
-
-        args.addRaw("-L");
-        args.addRaw("1");
+        ArgumentsBuilder args = getBaseArgs(true);
 
         File extendedReports = getTestFile("extendedReports.txt");
         args.addRaw("-arf");
         args.addRaw(normalizePath(extendedReports));
-
-        args.addRaw("-O");
-        args.addRaw("%s");
-        args.addRaw("--tmp-dir");
-        args.addRaw(getTmpDir());
 
         IntegrationTestSpec spec = new IntegrationTestSpec(
                 args.getString(), Arrays.asList(expected.getPath()));
@@ -213,5 +183,38 @@ public class VariantQCIntegrationTest extends BaseIntegrationTest {
         }
 
         return out;
+    }
+
+    @DataProvider(name = "testPedigreeValidationData")
+    private Object[][] testPedigreeValidationData() {
+        return new Object[][] {
+                new Object[]{PedigreeValidationType.SILENT, false},
+                new Object[]{PedigreeValidationType.STRICT, true},
+                new Object[]{null, true}
+        };
+    }
+
+    @Test(dataProvider = "testPedigreeValidationData")
+    public void testPedigreeValidation(PedigreeValidationType pvt, boolean expectFail) throws IOException {
+        String pedFile = "MendelianViolationEval.ped";
+        String name = "testPedigreeValidation";
+
+        ArgumentsBuilder args = getBaseArgs(true);
+        args.add("ped", normalizePath(new File(testBaseDir, pedFile)));
+
+        if (pvt != null) {
+            args.add("pedValidationType", pvt.name());
+        }
+
+        IntegrationTestSpec spec;
+        if (expectFail) {
+            spec = new IntegrationTestSpec(args.getString(), 1, UserException.class);
+        }
+        else {
+            File expected = generateCompleteOutput(getTestFile("testBasicOperation.html"));
+            spec = new IntegrationTestSpec(args.getString(), Collections.singletonList(expected.getPath()));
+        }
+
+        spec.executeTest(name, this);
     }
 }
