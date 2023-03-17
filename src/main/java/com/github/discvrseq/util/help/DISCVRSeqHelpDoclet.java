f2bc67d5ba189d1b221176ec1d876b3b9522aa6e
bbimber <bbimber@gmail.com>
2023-03-17 06:55:29 -0700

    Update docs

--- src/main/java/com/github/discvrseq/util/help/DISCVRSeqHelpDoclet.java
+++ src/main/java/com/github/discvrseq/util/help/DISCVRSeqHelpDoclet.java
@@ -1,7 +1,6 @@
 package com.github.discvrseq.util.help;
 
-import com.sun.javadoc.ClassDoc;
-import com.sun.javadoc.RootDoc;
+import jdk.javadoc.doclet.DocletEnvironment;
 import org.broadinstitute.barclay.help.DocWorkUnit;
 import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.barclay.help.GSONWorkUnit;
@@ -9,7 +8,7 @@ import org.broadinstitute.hellbender.utils.help.GATKDocWorkUnit;
 import org.broadinstitute.hellbender.utils.help.GATKHelpDocWorkUnitHandler;
 import org.broadinstitute.hellbender.utils.help.GATKHelpDoclet;
 
-import java.io.IOException;
+import javax.lang.model.element.Element;
 import java.util.List;
 import java.util.Map;
 
@@ -19,47 +18,23 @@ public class DISCVRSeqHelpDoclet extends GATKHelpDoclet {
 
     }
 
-    /**
-     * Create a doclet of the appropriate type and generate the FreeMarker templates properties.
-     * @param rootDoc
-     * @throws IOException
-     */
-    public static boolean start(final RootDoc rootDoc) throws IOException {
-        return new DISCVRSeqHelpDoclet().startProcessDocs(rootDoc);
+    public static boolean processDocs(final DocletEnvironment docletEnv) {
+        return new DISCVRSeqHelpDoclet().run(docletEnv);
     }
 
-    /**
-     * @return Create and return a DocWorkUnit-derived object to handle documentation
-     * for the target feature(s) represented by documentedFeature.
-     *
-     * @param documentedFeature DocumentedFeature annotation for the target feature
-     * @param classDoc javadoc classDoc for the target feature
-     * @param clazz class of the target feature
-     * @return DocWorkUnit to be used for this feature
-     */
     @Override
-    protected DocWorkUnit createWorkUnit(
-            final DocumentedFeature documentedFeature,
-            final ClassDoc classDoc,
-            final Class<?> clazz)
+    public DocWorkUnit createWorkUnit(
+            final Element classElement,
+            final Class<?> clazz,
+            final DocumentedFeature documentedFeature)
     {
         return new GATKDocWorkUnit(
                 new GATKHelpDocWorkUnitHandler(this),
-                documentedFeature,
-                classDoc,
-                clazz);
+                classElement,
+                clazz,
+                documentedFeature);
     }
 
-    /**
-     * Create a GSONWorkUnit-derived object that holds our custom data. This method should create the object, and
-     * propagate any custom javadoc tags from the template map to the newly created GSON object; specifically
-     * "walkertype", which is pulled from a custom javadoc tag.
-     *
-     * @param workUnit work unit for which a GSON object is required
-     * @param groupMaps
-     * @param featureMaps
-     * @return a GSONWorkUnit-derived object for this work unit, populated with any custom values
-     */
     @Override
     protected GSONWorkUnit createGSONWorkUnit(
             final DocWorkUnit workUnit,
