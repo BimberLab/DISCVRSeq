adcd0e6f7fafbe53bf7a5551278a11cfb3a67f4f
bbimber <bbimber@gmail.com>
2023-03-17 09:28:59 -0700

    Remove unnecessary code

--- src/main/java/com/github/discvrseq/util/help/DISCVRSeqHelpDoclet.java
+++ src/main/java/com/github/discvrseq/util/help/DISCVRSeqHelpDoclet.java
@@ -1,16 +1,7 @@
 package com.github.discvrseq.util.help;
 
 import jdk.javadoc.doclet.DocletEnvironment;
-import org.broadinstitute.barclay.help.DocWorkUnit;
-import org.broadinstitute.barclay.help.DocumentedFeature;
-import org.broadinstitute.barclay.help.GSONWorkUnit;
-import org.broadinstitute.hellbender.utils.help.GATKDocWorkUnit;
-import org.broadinstitute.hellbender.utils.help.GATKHelpDocWorkUnitHandler;
 import org.broadinstitute.hellbender.utils.help.GATKHelpDoclet;
-
-import javax.lang.model.element.Element;
-import java.util.List;
-import java.util.Map;
 
 @SuppressWarnings("removal")
 public class DISCVRSeqHelpDoclet extends GATKHelpDoclet {
