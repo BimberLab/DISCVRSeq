5e5c766c1495ca26e89f61e73f519d81d4f328bb
bbimber <bbimber@gmail.com>
2023-04-07 15:51:29 -0700

    Initial work to extend funcotator output

--- src/main/java/com/github/discvrseq/walkers/ExtendedFuncotator.java
+++ src/main/java/com/github/discvrseq/walkers/ExtendedFuncotator.java
@@ -15,15 +15,13 @@ import org.broadinstitute.barclay.help.DocumentedFeature;
 import org.broadinstitute.hellbender.engine.FeatureContext;
 import org.broadinstitute.hellbender.engine.ReferenceContext;
 import org.broadinstitute.hellbender.tools.funcotator.*;
+import org.broadinstitute.hellbender.tools.funcotator.dataSources.DataSourceUtils;
+import org.broadinstitute.hellbender.tools.funcotator.metadata.VcfFuncotationMetadata;
 import org.broadinstitute.hellbender.tools.funcotator.vcfOutput.VcfOutputRenderer;
 import org.broadinstitute.hellbender.utils.Utils;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.nio.file.Path;
+import java.util.*;
 
 /**
  * Create functional annotations on given variants cross-referenced by a given set of data sources.
@@ -35,18 +33,19 @@ import java.util.stream.Stream;
  *     -O output.annotated.vcf.gz
  * </pre>
  */
-//@CommandLineProgramProperties(
-//        summary = "Create functional annotations on given variants cross-referenced by a given set of data sources.\n" +
-//                "A GATK functional annotation tool (similar functionality to Oncotator).",
-//        oneLineSummary = "Functional Annotator",
-//        programGroup = DiscvrSeqProgramGroup.class
-//)
-//@DocumentedFeature
+@CommandLineProgramProperties(
+        summary = "Create functional annotations on given variants cross-referenced by a given set of data sources.\n" +
+                "A GATK functional annotation tool (similar functionality to Oncotator).",
+        oneLineSummary = "Functional Annotator",
+        programGroup = DiscvrSeqProgramGroup.class
+)
+@DocumentedFeature
 public class ExtendedFuncotator extends Funcotator {
     private static final Logger logger = LogManager.getLogger(ExtendedFuncotator.class);
 
     @Override
     public void onTraversalStart() {
+        addOutputVCFCommandLine = false;
 
         // Get our overrides for annotations:
         final LinkedHashMap<String, String> annotationDefaultsMap = FuncotatorEngine.splitAnnotationArgsIntoMap(getArguments().annotationDefaults);
@@ -54,6 +53,30 @@ public class ExtendedFuncotator extends Funcotator {
 
         // Get the header for our variants:
         final VCFHeader vcfHeader = getHeaderForVariants();
+
+        final Set<String> finalUserTranscriptIdSet = FuncotatorEngine.processTranscriptList(getArguments().userTranscriptIdSet);
+        final Map<Path, Properties> configData = DataSourceUtils.getAndValidateDataSourcesFromPaths( getArguments().referenceVersion,  getArguments().dataSourceDirectories);
+
+        final List<DataSourceFuncotationFactory> dataSourceFuncotationFactories = DataSourceUtils.createDataSourceFuncotationFactoriesForDataSources(
+                configData,
+                annotationOverridesMap,
+                getArguments().transcriptSelectionMode,
+                finalUserTranscriptIdSet,
+                this,
+                getArguments().lookaheadFeatureCachingInBp,
+                new FlankSettings(getArguments().fivePrimeFlankSize, getArguments().threePrimeFlankSize),
+                false,
+                getArguments().minNumBasesForValidSegment
+        );
+
+        funcotatorEngine = new FuncotatorEngine(
+                getArguments(),
+                getSequenceDictionaryForDrivingVariants(),
+                VcfFuncotationMetadata.create(
+                        new ArrayList<>(vcfHeader.getInfoHeaderLines())
+                ),
+                dataSourceFuncotationFactories
+        );
 
         // Create our output renderer:
         logger.info("Creating a " + getArguments().outputFormatType + " file for output: " + getArguments().outputFile.toURI());
@@ -121,11 +144,11 @@ public class ExtendedFuncotator extends Funcotator {
                     funcotatorAnnotationStringBuilder.append(FIELD_DELIMITER);
                 }
 
-//                for (final String txId : txToFuncotationMap.getTranscriptList()) {
-//                    funcotatorAnnotationStringBuilder.append(START_TRANSCRIPT_DELIMITER);
-//                    final List<Funcotation> funcotations = txToFuncotationMap.get(txId);
-//                    final Funcotation manualAnnotationFuncotation = createManualAnnotationFuncotation(altAllele);
-//
+                for (final String txId : txToFuncotationMap.getTranscriptList()) {
+                    funcotatorAnnotationStringBuilder.append(START_TRANSCRIPT_DELIMITER);
+                    final List<Funcotation> funcotations = txToFuncotationMap.get(txId);
+                    final Funcotation manualAnnotationFuncotation = OutputRenderer.createFuncotationFromLinkedHashMap(manualAnnotations, altAllele, "UnaccountedManualAnnotations");
+
 //                    funcotatorAnnotationStringBuilder.append(
 //                            Stream.concat(funcotations.stream(), Stream.of(manualAnnotationFuncotation))
 //                                    .filter(f -> f.getAltAllele().equals(altAllele))
@@ -135,12 +158,12 @@ public class ExtendedFuncotator extends Funcotator {
 //                                    .map(f -> FuncotatorUtils.renderSanitizedFuncotationForVcf(f, finalFuncotationFieldNames))
 //                                    .collect(Collectors.joining(FIELD_DELIMITER))
 //                    );
-//
-//                    funcotatorAnnotationStringBuilder.append(END_TRANSCRIPT_DELIMITER + ALL_TRANSCRIPT_DELIMITER);
-//                }
-//                // We have a trailing "#" - we need to remove it:
-//                funcotatorAnnotationStringBuilder.deleteCharAt(funcotatorAnnotationStringBuilder.length()-1);
-//                funcotatorAnnotationStringBuilder.append(VCFConstants.INFO_FIELD_ARRAY_SEPARATOR);
+
+                    funcotatorAnnotationStringBuilder.append(END_TRANSCRIPT_DELIMITER + ALL_TRANSCRIPT_DELIMITER);
+                }
+                // We have a trailing "#" - we need to remove it:
+                funcotatorAnnotationStringBuilder.deleteCharAt(funcotatorAnnotationStringBuilder.length()-1);
+                funcotatorAnnotationStringBuilder.append(VCFConstants.INFO_FIELD_ARRAY_SEPARATOR);
             }
 
             // We have a trailing "," - we need to remove it:
---
+++ src/test/java/com/github/discvrseq/walkers/ExtendedFuncotatorIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.github.discvrseq.walkers;
+
+import org.broadinstitute.hellbender.testutils.ArgumentsBuilder;
+import org.broadinstitute.hellbender.testutils.IntegrationTestSpec;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class ExtendedFuncotatorIntegrationTest extends  BaseIntegrationTest {
+
+    @Test
+    public void testBasicOperation() throws Exception {
+        ArgumentsBuilder args = getBaseArgs();
+
+        args.addRaw("--variant");
+        File input = new File(testBaseDir, "simpleTest.vcf");
+        ensureVcfIndex(input);
+        args.addRaw(normalizePath(input));
+
+        IntegrationTestSpec spec = new IntegrationTestSpec(
+                args.getString(),
+                Arrays.asList(
+                    getTestFile("funcotator1.vcf").getPath()
+                ));
+
+        spec.executeTest("testBasicOperation", this);
+    }
+
+    private ArgumentsBuilder getBaseArgs() {
+        ArgumentsBuilder args = new ArgumentsBuilder();
+        args.add("R", normalizePath(getHg19Micro()));
+        args.add("ref-version", "hg19");
+
+        args.add("data-sources-path", normalizePath(new File(testBaseDir, "funcotator")));
+        args.add("output-file-format", "VCF");
+
+        args.addRaw("-O");
+        args.addRaw("%s");
+        args.addRaw("--tmp-dir");
+        args.addRaw(getTmpDir());
+
+        return args;
+    }
+}
