a108c818f78dac9ba3bccf61f50b122bd7d83e6d
bbimber <bbimber@gmail.com>
2025-03-03 14:36:23 -0800

    Improve SVType annotation and add testing

--- src/main/java/com/github/discvrseq/walkers/annotator/SVType.java
+++ src/main/java/com/github/discvrseq/walkers/annotator/SVType.java
@@ -6,26 +6,62 @@ import htsjdk.variant.vcf.VCFCompoundHeaderLine;
 import org.broadinstitute.hellbender.engine.ReferenceContext;
 import org.broadinstitute.hellbender.tools.spark.sv.utils.GATKSVVCFConstants;
 import org.broadinstitute.hellbender.tools.spark.sv.utils.GATKSVVCFHeaderLines;
-import org.broadinstitute.hellbender.tools.sv.SVCallRecordUtils;
 import org.broadinstitute.hellbender.tools.walkers.annotator.InfoFieldAnnotation;
 import org.broadinstitute.hellbender.utils.genotyper.AlleleLikelihoods;
 import org.broadinstitute.hellbender.utils.read.GATKRead;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class SVType implements InfoFieldAnnotation {
+    private static final Set<String> VALID_TYPES = new HashSet<>(Arrays.asList(GATKSVVCFConstants.StructuralVariantAnnotationType.values()).stream()
+            .map(GATKSVVCFConstants.StructuralVariantAnnotationType::name).collect(Collectors.toList()));
+
     @Override
     public Map<String, Object> annotate(ReferenceContext ref, VariantContext vc, AlleleLikelihoods<GATKRead, Allele> likelihoods) {
-        GATKSVVCFConstants.StructuralVariantAnnotationType type = SVCallRecordUtils.inferStructuralVariantType(vc);
-        if (type == null) {
+        if (!vc.isVariant()) {
             return Collections.emptyMap();
         }
 
+        if (vc.getType() == VariantContext.Type.SNP || vc.getType() == VariantContext.Type.MNP) {
+            return Collections.emptyMap();
+        }
+
+        Set<GATKSVVCFConstants.StructuralVariantAnnotationType> types = new HashSet<>();
+        for (Allele a : vc.getAlternateAlleles()) {
+            if (vc.isSimpleInsertion()) {
+                types.add(GATKSVVCFConstants.StructuralVariantAnnotationType.INS);
+            }
+            else if (vc.isSimpleDeletion()) {
+                types.add(GATKSVVCFConstants.StructuralVariantAnnotationType.DEL);
+            }
+            if (a.isSymbolic()) {
+                final String alleleType = a.getDisplayString().replace("<", "").replace(">", "");
+                if (VALID_TYPES.contains(alleleType)) {
+                    types.add(GATKSVVCFConstants.StructuralVariantAnnotationType.valueOf(alleleType));
+                } else {
+                    throw new IllegalArgumentException("Could not find a valid SV type for variant " + vc.getID());
+                }
+            }
+            else if (a.isBreakpoint()){
+                types.add(GATKSVVCFConstants.StructuralVariantAnnotationType.BND);
+            }
+            else if (a.length() > vc.getReference().length()) {
+                types.add(GATKSVVCFConstants.StructuralVariantAnnotationType.INS);
+            }
+            else if (a.length() < vc.getReference().length()) {
+                types.add(GATKSVVCFConstants.StructuralVariantAnnotationType.DEL);
+            }
+        }
+
         Map<String, Object> attributeMap = new HashMap<>();
-        attributeMap.put(GATKSVVCFConstants.SVTYPE, type.name());
+        if (types.size() == 1) {
+            String type = types.stream().map(GATKSVVCFConstants.StructuralVariantAnnotationType::name).findFirst().get();
+            attributeMap.put(GATKSVVCFConstants.SVTYPE, type);
+        }
+        else if (types.size() > 1) {
+            attributeMap.put(GATKSVVCFConstants.SVTYPE, GATKSVVCFConstants.StructuralVariantAnnotationType.CPX);
+        }
 
         return attributeMap;
     }
